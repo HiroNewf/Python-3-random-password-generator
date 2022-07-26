21afc869eac3ee749993724a7f50469842db84ae
HiroNewf <64501695+HiroNewf@users.noreply.github.com>
2022-07-26 17:15:35 -0400

    The script.py
    Changed variable name to make more sense

--- Script.py
+++ Script.py
@@ -7,18 +7,18 @@ password_numbers = input("Do you want to include numbers in the password? (yes/n
 capitol_letters = input("Do you want to include capitol letters in the password? (yes/no): ")
 
 if password_numbers == "yes" and capitol_letters == "yes":
-    res = ''.join(random.choices(string.ascii_letters + string.digits, k=password_length))
+    the_password = ''.join(random.choices(string.ascii_letters + string.digits, k=password_length))
 
 elif password_numbers == "no" and capitol_letters == "yes":
-    res = ''.join(random.choices(string.ascii_letters, k=password_length))
+    the_password = ''.join(random.choices(string.ascii_letters, k=password_length))
     
 elif password_numbers == "yes" and capitol_letters == "no":
-    res = ''.join(random.choices(string.digits, + string.ascii_lowercase, k=password_length))
+    the_password = ''.join(random.choices(string.digits, + string.ascii_lowercase, k=password_length))
     
 elif password_numbers == "no" and capitol_letters == "no":
-    res = ''.join(random.choices(k=password_length))
+    the_password = ''.join(random.choices(k=password_length))
     
 else:
     print('You have not entered valid responses, please try again')
 
-print("Your password is: " + str(res))
+print("Your password is: " + str(the_password))
