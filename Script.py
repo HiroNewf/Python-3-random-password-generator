e0492ce1d2714479e0a8c75b9bcfb8fb0b03c0cf
HiroNewf <64501695+HiroNewf@users.noreply.github.com>
2022-07-26 17:17:46 -0400

    Myscript.py

--- Script.py
+++ Script.py
@@ -8,17 +8,21 @@ capitol_letters = input("Do you want to include capitol letters in the password?
 
 if password_numbers == "yes" and capitol_letters == "yes":
     the_password = ''.join(random.choices(string.ascii_letters + string.digits, k=password_length))
-
+    print("Your password is: " + str(the_password))
+    
 elif password_numbers == "no" and capitol_letters == "yes":
     the_password = ''.join(random.choices(string.ascii_letters, k=password_length))
-    
+    print("Your password is: " + str(the_password))
+
 elif password_numbers == "yes" and capitol_letters == "no":
     the_password = ''.join(random.choices(string.digits, + string.ascii_lowercase, k=password_length))
-    
+    print("Your password is: " + str(the_password))
+
 elif password_numbers == "no" and capitol_letters == "no":
     the_password = ''.join(random.choices(k=password_length))
-    
+    print("Your password is: " + str(the_password))
+
 else:
     print('You have not entered valid responses, please try again')
 
-print("Your password is: " + str(the_password))
+
