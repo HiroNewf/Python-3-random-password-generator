d97f599384077ed327baea8eebcd6feb083b31c4
HiroNewf <64501695+HiroNewf@users.noreply.github.com>
2022-07-26 17:14:02 -0400

    Update Script.py

--- Script.py
+++ Script.py
@@ -2,9 +2,9 @@ import string
 import random
 
 # settings
-password_length = int(input('What is the length you want the password to be?: '))
+password_length = int(input('What is the length you want the password to be? (A digit): '))
 password_numbers = input("Do you want to include numbers in the password? (yes/no): ")
-capitol_letters = input("Do you want to include capitol letters in the password? (yes/no: ")
+capitol_letters = input("Do you want to include capitol letters in the password? (yes/no): ")
 
 if password_numbers == "yes" and capitol_letters == "yes":
     res = ''.join(random.choices(string.ascii_letters + string.digits, k=password_length))
